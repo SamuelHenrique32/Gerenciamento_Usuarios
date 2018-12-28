a0bccbb0f04cd59fab832f241b14a065a9a87c8a
SamuelHenrique32 <shdalmas@ucs.br>
2018-12-28 01:19:20 -0200

    Aula 37 - POO Classes e MVC

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ var user = {};
 
 function addLine(dataUser){
 
-    var tr = document.createElement("tr");
+    console.log(dataUser);
 
     //comando HTML
-    tr.innerHTML = `
+    document.getElementById("table-users").innerHTML = `
                     <tr>
                         <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
                         <td>${dataUser.name}</td>
@@ -19,9 +19,6 @@ function addLine(dataUser){
                         </td>
                   </tr>
     `;
-
-    document.getElementById("table-users").appendChild(tr);
-
 
 }
 
@@ -42,5 +39,16 @@ document.getElementById("form-user-create").addEventListener("submit", function 
 
     });
 
-    addLine(user);
+    //Json -> classe User
+    var objectUser = new User(
+        user.name,
+        user.gender,
+        user.birth,
+        user.country,
+        user.email,
+        user.password,
+        user.photo,
+        user.admin);
+
+    addLine(objectUser);
 });
