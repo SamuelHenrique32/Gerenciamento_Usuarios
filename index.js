e2db45317795241d8ccb6950bd10c67e9544c929
SamuelHenrique32 <shdalmas@ucs.br>
2018-12-27 23:29:48 -0200

    Aula 34 - Trabalhando com Json

--- index.js
+++ index.js
@@ -1,25 +1,17 @@
-var name = document.querySelector("#exampleInputName");                                 //referencia para HTML, elemento (# do CSS3)
-var gender = document.querySelectorAll("#form-user-create [name=gender]:checked");      //traz o marcado
-var birth = document.querySelector("#exampleInputBirth");
-var country = document.querySelector("#exampleInputCountry");
-var email = document.querySelector("#exampleInputEmail");
-var password = document.querySelector("#exampleInputPassword");
-var photo = document.querySelector("#exampleInputFile");
-var admin = document.querySelector("#exampleInputAdmin");
-
 var fields = document.querySelectorAll("#form-user-create [name]");                     //tras os que tem name
+var user = {};
 
 fields.forEach(function (field, index) {                                                //para cada campo do formulario
 
     if(field.name == "gender"){
 
         if(field.checked){                                                               //true
-            console.log("SIM", field);
+            user[field.name] = field.value;
         }
     } else{
-        console.log("NÃO")
+        user[field.name] = field.value;
     }
 
-    //console.log(field.id, field.name, field.value, field.checked, index);
-
 });
+
+console.log(user);
