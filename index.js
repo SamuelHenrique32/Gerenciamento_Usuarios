39c8f4736287348e6b968ce5ea662bdbf57c09dd
SamuelHenrique32 <shdalmas@ucs.br>
2018-12-28 00:41:29 -0200

    Aula 36 - Trabalhando com Funcoes

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 var fields = document.querySelectorAll("#form-user-create [name]");                     //tras os que tem name
 var user = {};
 
+function addLine(dataUser){
+
+    var tr = document.createElement("tr");
+
+    //comando HTML
+    tr.innerHTML = `
+                    <tr>
+                        <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+                        <td>${dataUser.name}</td>
+                        <td>${dataUser.email}</td>
+                        <td>${dataUser.admin}</td>
+                        <td>${dataUser.birth}</td>
+                        <td>
+                          <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                          <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                        </td>
+                  </tr>
+    `;
+
+    document.getElementById("table-users").appendChild(tr);
+
+
+}
+
 document.getElementById("form-user-create").addEventListener("submit", function (event) {
     //cancela comportamento padrao, nao manda mais via get
     event.preventDefault();
@@ -18,5 +42,5 @@ document.getElementById("form-user-create").addEventListener("submit", function 
 
     });
 
-    console.log(user);
+    addLine(user);
 });
