c220d846e7f2412bc9f2121d94ecef0232d5ab2b
SamuelHenrique32 <shdalmas@ucs.br>
2018-12-27 22:18:59 -0200

    Aula 31 - Variaveis e Seletores

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-var nome = document.querySelector("#exampleInputName");      //referencia para HTML, elemento (# do CSS3)
+var name = document.querySelector("#exampleInputName");                                 //referencia para HTML, elemento (# do CSS3)
+var gender = document.querySelectorAll("#form-user-create [name=gender]:checked");      //traz o marcado
+var birth = document.querySelector("#exampleInputBirth");
+var country = document.querySelector("#exampleInputCountry");
+var email = document.querySelector("#exampleInputEmail");
+var password = document.querySelector("#exampleInputPassword");
+var photo = document.querySelector("#exampleInputFile");
+var admin = document.querySelector("#exampleInputAdmin");
